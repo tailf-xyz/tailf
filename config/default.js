d19f5983b53199c8d6c46cdcebafec3c0ecc2f3f
Ahmed Kamel <ahmed.kamel@gmail.com>
2018-11-28 23:18:13 -0800

    cleanup and respect expirein and iss

--- config/default.js
+++ config/default.js
@@ -3,33 +3,34 @@ var fs    = require("fs"),
     ms    = require("ms");
 
 module.exports = {
-  jwt: {
-    key: null,
-    public: null,
-    expiresIn: ms("4h") / 1000
+  "jwt": {
+    "key"       : null,
+    "public"    : null,
+    "expiresIn" : "12h",
+    "iss"       : "tailf.io"
   },
-  tailf: {
-    port: 8654,
-    origin: "https://tailf.io",
-    ls_limit: 50,
-    log: {
-      dirname: "./disk"
+  "tailf": {
+    "port": 8654,
+    "origin": "https://tailf.io",
+    "ls_limit": 50,
+    "log": {
+      "dirname": "/disk"
     },
-    chunk: {
-      size: bytes("50kb")
+    "chunk": {
+      "size": bytes('50kb')
     },
-    redis: {
-      host: "localhost",
-      port: 6379,
-      password: null,
-      db: 0,
-      db_index: 1
+    "redis": {
+      "host": "localhost",
+      "port": 6379,
+      "password": null,
+      "db": 0,
+      "db_index": 1
     },
-    metering: {
+    "metering": {
       "*": {
-        limit_per_file: bytes("100kb"),
-        limit_metadata: bytes("20kb"),
-        ttl: ms("15d")
+        "limit_per_file": bytes('100kb'),
+        "limit_metadata": bytes('20kb'),
+        "ttl": ms('15d')
       }
     }
   }
